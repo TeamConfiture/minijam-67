53ca44d21d1028941e07ed4fcb8b570c7b20a9fc
Lugrim <Lugrim@users.noreply.github.com>
2020-11-14 13:31:42 +0100

    Added an empty Game Menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Quit");
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Scripts/Menu/MainMenuScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject CreditsMenu;
+    public AudioSource AudioSource;
+
+    public AudioClip click;
+    public AudioClip hover;
+
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+
+    }
+
+    public void PlayNowButton(string scene) {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
+        AudioSource.Play();
+    }
+    public void CreditsButton() {
+        CreditsMenu.SetActive(true);
+        MainMenu.SetActive(false);
+        AudioSource.Play();
+    }
+    public void MainMenuButton() {
+        MainMenu.SetActive(true);
+        CreditsMenu.SetActive(false);
+    }
+    public void BackButton(){
+        MainMenu.SetActive(true);
+        CreditsMenu.SetActive(false);
+        AudioSource.Play();
+    }
+    public void QuitButton() {
+        Application.Quit();
+    }
+
+    public void HoverSound()
+    {
+        AudioSource.PlayOneShot(hover);
+    }
+    
+}
