341c97486c0365311c2a5c26b79e002ed25c37f8
Lugrim <Lugrim@users.noreply.github.com>
2020-11-14 15:01:24 +0100

    Added minigame-manager

---
+++ Assets/Scripts/Minigames/DummyMinigameScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyMinigameScript : MonoBehaviour
+{
+    public MGManagerScript manager;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnMouseDown() {
+        manager.NextGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Minigames/MGManagerScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MGManagerScript : MonoBehaviour
+{
+    
+    public List<GameObject> minigames;
+
+    public Text chronoText;
+
+    
+    private bool playing;
+    private double chronoTime;
+    private int currentGame = -1;
+
+    public void NextGame() {
+        currentGame++;
+        // currentGame = currentGame % minigames.Count;
+        if(currentGame < minigames.Count - 1) {
+            SetMiniGame(currentGame);
+        } else {
+            EndMiniGames();
+        }
+    }
+
+    private void SetMiniGame(int i) {
+        minigames.FindAll(g => g.activeSelf).ForEach(g => g.SetActive(false));
+        minigames[i].SetActive(true);
+    }
+
+    private void EndMiniGames() {
+        minigames.FindAll(g => g.activeSelf).ForEach(g => g.SetActive(false));
+        playing = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playing = true;
+        if(minigames.Count < 1) {
+            Debug.LogError("No minigame found !");
+            return;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playing) {
+            chronoTime += Time.deltaTime;
+            chronoText.text = chronoTime.ToString("F");
+        }
+    }
+}
