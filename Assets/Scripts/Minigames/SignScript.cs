e434e20cc38103d46736ce38a108cfad4266cbc5
Metatheria <farina@etud.insa-toulouse.fr>
2020-11-15 19:49:14 +0100

    mise à jour de SignScript, ajout du scrolling dans le minijeu lorem ipsum

--- Assets/Scripts/Minigames/SignScript.cs
+++ Assets/Scripts/Minigames/SignScript.cs
@@ -9,25 +9,25 @@ public class SignScript : MonoBehaviour
     public float objective;
     Vector3 oldPos = Vector3.zero;
     bool writing;
-    public GameObject signatureSprite;
+    //public GameObject signatureSprite;
     SpriteRenderer theRenderer;
     public MGManagerScript managerScript;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (signatureSprite != null) {
-            theRenderer = signatureSprite.GetComponent<SpriteRenderer>();
+		//if (signatureSprite != null) {
+            theRenderer = gameObject.GetComponent<SpriteRenderer>();
             theRenderer.color = new Color (1, 1, 1, 0); 
-        } else {
-            Debug.Log("No Signature attached !");
-        }
+			//} else {
+         //   Debug.Log("No Signature attached !");
+			//}
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1")) {
+		if (Input.GetButtonDown("Fire1")) {
             oldPos = Input.mousePosition;
             writing = true;
             Debug.Log("Started to write");
@@ -38,7 +38,12 @@ public class SignScript : MonoBehaviour
                 oldPos = Vector3.zero;
             }
         }
-        if (writing) {
+        
+    }
+	
+	void OnMouseOver()
+	{
+		if (writing) {
             Vector3 newPos =  Input.mousePosition;
             if (newPos.x < 0 || newPos.y < 0 || newPos.x > Screen.width || newPos.y > Screen.height) {
                 writing = false;
@@ -54,7 +59,7 @@ public class SignScript : MonoBehaviour
                 }
             }
         }
-    }
+	}
 
     void Objective() {
         Debug.Log("Document signed !");
