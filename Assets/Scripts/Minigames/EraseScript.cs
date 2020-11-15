bd14f6551fe8f1340f1f55e953c682b526abd7a6
Metatheria <farina@etud.insa-toulouse.fr>
2020-11-15 20:49:05 +0100

    WIP lorem ipsum mini game manager

---
+++ Assets/Scripts/Minigames/EraseScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EraseScript : MonoBehaviour
+{
+    float reduceFactor = 1000;
+    float progress;
+    public float objective;
+    Vector3 oldPos = Vector3.zero;
+    bool writing;
+    //public GameObject signatureSprite;
+    SpriteRenderer theRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            theRenderer = gameObject.GetComponent<SpriteRenderer>();
+            theRenderer.color = new Color (1, 1, 1, 0); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetButtonDown("Fire1")) {
+            oldPos = Input.mousePosition;
+            writing = true;
+            Debug.Log("Started to write");
+        } else if (Input.GetButtonUp("Fire1")) {
+            if (writing) {
+                writing = false;
+                Debug.Log("Stopped writing");
+                oldPos = Vector3.zero;
+            }
+        }
+        
+    }
+	
+	void OnMouseOver()
+	{
+		if (writing) {
+            Vector3 newPos =  Input.mousePosition;
+            if (newPos.x < 0 || newPos.y < 0 || newPos.x > Screen.width || newPos.y > Screen.height) {
+                writing = false;
+            } else {
+                float delta = Vector3.Distance(oldPos, newPos)*Time.deltaTime / reduceFactor;
+                progress += delta;
+                theRenderer.color = new Color (1, 1, 1, progress/objective); 
+                Debug.Log("Current : "+progress+"; delta : "+delta+"; objective : "+objective);
+                if (progress >= objective) {
+                    Objective();
+                    writing = false;
+                }
+            }
+        }
+	}
+
+    void Objective() {
+        Debug.Log("Document signed !");
+    }
+}
--- Assets/Scripts/Minigames/LoremIpsumScrolling.cs
+++ Assets/Scripts/Minigames/LoremIpsumScrolling.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class LoremIpsumScrolling : MonoBehaviour
 {
+	int nbWordsErased = 0;
+	const int TOTAL_WORDS = 11;
+	float distanceScrolled = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,24 @@ public class LoremIpsumScrolling : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + new Vector3(0,0.0075f, 0);
+        transform.position = transform.position + new Vector3(0,1.2f, 0)*Time.deltaTime;
+		distanceScrolled += 1.2f*Time.deltaTime;
+		if(distanceScrolled >= 48.0f)// hauteur du TextMeshPro, hardcodée (je sais c'est pas beau)
+			EndGame();
     }
+	
+	public void WordErased()
+	{
+		nbWordsErased++;
+		if(nbWordsErased == TOTAL_WORDS)
+			EndGame();
+	}
+	
+	void EndGame()
+	{
+		if(nbWordsErased == TOTAL_WORDS)
+			Debug.Log("Success!");//passer au minijeu suivant
+		else
+			Debug.Log("Failure!");//reset le minijeu
+	}
 }
