dcd9852d6fcdfc8e14eecc5c0b6fab5efdf91096
Lugrim <Lugrim@users.noreply.github.com>
2020-11-16 00:00:56 +0100

    Added Chrono Screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
-    private double Chrono;
+    public double Chrono;
 
     void Awake()
     {
---
+++ Assets/Scripts/WinScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class WinScreen : MonoBehaviour
+{
+    public string nextScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<TextMeshProUGUI>().text = "You won in " + GameManager.Instance.Chrono.ToString("F") + " seconds ! (ending scene in 3 seconds)";
+        StartCoroutine(ChangeScene(3));
+    }
+
+ IEnumerator ChangeScene(int index,float delay = 2f)
+     {
+          yield return new WaitForSeconds(delay);
+          SceneManager.LoadScene(nextScene);
+     }
+}
